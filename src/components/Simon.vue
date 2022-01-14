7347d371787ee1c489cb2256a94ee2c327878809
Alex <alexmcgil@vivaldi.net>
2022-01-14 09:51:46 +0300

    Finished the logic of the game. Added comments

--- src/components/Simon.vue
+++ src/components/Simon.vue
@@ -1,39 +1,33 @@
 <template>
-  <div>
+  <div class="wrapper">
     <div class="game">
       <div class="simon">
         <ul v-on:click="action">
-<!--        <ul v-on:click="blink">-->
-          <li  class="tile red"></li>
+          <li class="tile red"></li>
           <li class="tile blue"></li>
           <li class="tile yellow"></li>
           <li class="tile green"></li>
         </ul>
       </div>
     </div>
-    <div class="info">
-      <button v-on:click="startGame">Играть!</button>
-      <p>Вы проиграли :(</p>
-    </div>
-    <div class="difficulty">
-      <h2>Сложность:</h2>
-      <input type="radio" v-model="difficulty" name="mode" value="easy" id="easy" checked><label
-        for="easy">Лёгкая</label><br>
-      <input type="radio" v-model="difficulty" name="mode" value="normal" id="normal"><label
-        for="normal">Средняя</label><br>
-      <input type="radio" v-model="difficulty" name="mode" value="hard" id="hard"><label for="hard">Сложная</label>
+    <div class="descriptions">
+      <div class="info">
+        <button v-on:click="startGame" :disabled="block">Играть!</button>
+        <p v-if="lose">Вы проиграли :(</p>
+      </div>
+      <div class="difficulty">
+        <h2>Сложность:</h2>
+        <input type="radio" v-model="difficulty" name="mode" value="easy" id="easy" checked><label
+          for="easy">Лёгкая</label><br>
+        <input type="radio" v-model="difficulty" name="mode" value="normal" id="normal"><label
+          for="normal">Средняя</label><br>
+        <input type="radio" v-model="difficulty" name="mode" value="hard" id="hard"><label for="hard">Сложная</label>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-
-let soundOne = new Audio(require("./../../sounds/1.mp3"))
-let soundTwo = new Audio(require("./../../sounds/2.mp3"))
-let soundThree = new Audio(require("./../../sounds/3.mp3"))
-let soundFour = new Audio(require("./../../sounds/4.mp3"))
-
-
 export default {
   name: 'Simon',
   data: () => {
@@ -41,14 +35,17 @@ export default {
       difficulty: "easy",
       playerSteps: [],
       simonsSteps: [],
-      sequence: [],
+      lose: false,
+      isGame: false,
+      block: false,
       step: 0,
     }
   },
+
   methods: {
 
-    getDifficulty(){
-      switch (this.difficulty){
+    getDifficulty() {   // Simple switcher difficulty
+      switch (this.difficulty) {
         case "easy":
           return 1500;
         case "normal":
@@ -59,38 +56,60 @@ export default {
     },
 
     action(e) {
-      if (this.isPlayer(e)){
+      if (this.isPlayer(e)) {   // If the action is performed by a player, the first condition checks it.
         this.playSound(e.target.className)
         this.blink(e)
         this.playerSteps.push(e.target.className)
+        if (this.step == this.simonsSteps.length - 1) { // If the player repeated Simon's moves
+          this.playerSteps = []                         // with his own moves, then his sequence
+          this.step = 0                                 // is cleared, and Simon randomly chooses his next move
+          let that = this
+
+          setTimeout(function () {
+            return function () {
+              that.Simon()
+            };
+          }(), 1000);
+
+          // If the player repeats Simon's moves, then everything is OK 👌
+        } else if (this.playerSteps[this.step] == this.simonsSteps[this.step]) {
+          this.step += 1
+        } else {                // Else game is over 😞
+          this.block = false
+          if (this.isGame) {    // Checking for clicks without playing
+            this.lose = true
+          }
+        }
       } else {
-        let pieces = document.querySelectorAll("li")
-        console.log(e)
+            // Else Simon gets all the tiles
+        let tiles = document.querySelectorAll("li")
         switch (e) {
+                              // Checks which tile matches his new random move
           case "tile red":
-            this.blink(pieces[0]);
+            this.blink(tiles[0]); // And plays an animation with sound
             this.playSound(e)
             break;
 
-            case "tile blue":
-            this.blink(pieces[1]);
-              this.playSound(e)
+          case "tile blue":
+            this.blink(tiles[1]);
+            this.playSound(e)
             break;
 
-            case "tile yellow":
-            this.blink(pieces[2]);
-              this.playSound(e)
+          case "tile yellow":
+            this.blink(tiles[2]);
+            this.playSound(e)
             break;
 
-            case "tile green":
-            this.blink(pieces[3]);
-              this.playSound(e)
+          case "tile green":
+            this.blink(tiles[3]);
+            this.playSound(e)
             break;
         }
       }
     },
 
-    getRandomTile() {
+    getRandomTile() {     // Simple randomizer to get string of tile for Simon
+
       switch (Math.floor(Math.random() * (4 - 1 + 1)) + 1) {
         case 1:
           return "tile red"
@@ -105,10 +124,15 @@ export default {
           return "tile green"
 
       }
-
     },
 
     playSound(step) {
+
+      let soundOne = new Audio(require("./../../sounds/1.mp3"))   // Import sounds
+      let soundTwo = new Audio(require("./../../sounds/2.mp3"))
+      let soundThree = new Audio(require("./../../sounds/3.mp3"))
+      let soundFour = new Audio(require("./../../sounds/4.mp3"))
+
       switch (step) {
         case "tile red":
           soundOne.play()
@@ -129,50 +153,65 @@ export default {
       }
     },
 
-    blink(stepOrEvent) {
+    blink(stepOrEvent) {  // Just simple blinking that uses CSS
       let area
+
+      // Due to the fact that the event
+      // is slightly different from
+      // the player and Simon, you need
+      // to check who wants to make a blink
+
       if (this.isPlayer(stepOrEvent)) {
         area = stepOrEvent.target;
-        console.log(area)
-      }else{
+      } else {
         area = stepOrEvent
-        console.log(area)
       }
-      console.log(area)
-      area.classList.add("blink")
-      setTimeout(function() {
-        return function() {
-          area.classList.toggle("blink");
+      area.style.opacity = "1"
+      setTimeout(function () {
+        return function () {
+          area.style.opacity = "0.6"
         };
       }(), 150);
     },
 
-    isPlayer(e) {
-      switch (e.target){
+    isPlayer(e) {         // Checking if it's a player or Simon
+      switch (e.target) {
         case undefined:
           return false
+
         default:
           return true
+
       }
     },
 
-        startGame() {
-      // this.playerSteps = []
-      // this.simonsSteps = []
+    startGame() {
+      this.block = true     // Blocking the game start button
+      this.step = 0         // Zeroing counters
+      this.playerSteps = []
+      this.simonsSteps = []
+      this.isGame = true    // Checking whether the game is in progress so as not
+                            // to issue a loss message when the player
+                            // simply clicks on the playing field
 
-      this.Simon()
+      this.Simon()          // Initializing the game, Simon makes the first move
     },
-    Simon(){
+    Simon() {
+
       let that = this;
       let bup = that.getRandomTile()
       that.simonsSteps.push(bup)
       let steps = that.simonsSteps
-      for(let currentIndex=0; currentIndex<that.simonsSteps.length; currentIndex++) {
-        setTimeout(function() {
-          return function() {
-            that.action(bup)
+
+      for (let currentIndex = 0; currentIndex < steps.length; currentIndex++) {
+
+        setTimeout(function () {
+          return function () {
+
+            that.action(steps[currentIndex])
+
           };
-        }(currentIndex), that.getDifficulty()*(currentIndex));
+        }(currentIndex), that.getDifficulty() * (currentIndex));
       }
     }
   }
