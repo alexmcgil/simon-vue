f361522e06b35b8eed1a84c9c205d7f7e55d6ecc
Alex <alexmcgil@vivaldi.net>
2022-01-14 08:09:26 +0300

    Sound playback and blinking works for both the player and Simon

--- src/components/Simon.vue
+++ src/components/Simon.vue
@@ -2,8 +2,9 @@
   <div>
     <div class="game">
       <div class="simon">
-        <ul v-on:click="playSound">
-          <li class="tile red"></li>
+        <ul v-on:click="action">
+<!--        <ul v-on:click="blink">-->
+          <li  class="tile red"></li>
           <li class="tile blue"></li>
           <li class="tile yellow"></li>
           <li class="tile green"></li>
@@ -16,8 +17,10 @@
     </div>
     <div class="difficulty">
       <h2>Сложность:</h2>
-      <input type="radio" v-model="difficulty" name="mode" value="easy" id="easy" checked><label for="easy">Лёгкая</label><br>
-      <input type="radio" v-model="difficulty" name="mode" value="normal" id="normal"><label for="normal">Средняя</label><br>
+      <input type="radio" v-model="difficulty" name="mode" value="easy" id="easy" checked><label
+        for="easy">Лёгкая</label><br>
+      <input type="radio" v-model="difficulty" name="mode" value="normal" id="normal"><label
+        for="normal">Средняя</label><br>
       <input type="radio" v-model="difficulty" name="mode" value="hard" id="hard"><label for="hard">Сложная</label>
     </div>
   </div>
@@ -30,21 +33,83 @@ let soundTwo = new Audio(require("./../../sounds/2.mp3"))
 let soundThree = new Audio(require("./../../sounds/3.mp3"))
 let soundFour = new Audio(require("./../../sounds/4.mp3"))
 
-function getRandomInRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 
 export default {
   name: 'Simon',
   data: () => {
     return {
-      difficulty: "easy"
+      difficulty: "easy",
+      playerSteps: [],
+      simonsSteps: [],
+      sequence: [],
+      step: 0,
     }
   },
   methods: {
-    playSound(e) {
-      switch (e.target.className) {
+
+    getDifficulty(){
+      switch (this.difficulty){
+        case "easy":
+          return 1500;
+        case "normal":
+          return 1000;
+        case "hard":
+          return 400;
+      }
+    },
+
+    action(e) {
+      if (this.isPlayer(e)){
+        this.playSound(e.target.className)
+        this.blink(e)
+        this.playerSteps.push(e.target.className)
+      } else {
+        let pieces = document.querySelectorAll("li")
+        console.log(e)
+        switch (e) {
+          case "tile red":
+            this.blink(pieces[0]);
+            this.playSound(e)
+            break;
+
+            case "tile blue":
+            this.blink(pieces[1]);
+              this.playSound(e)
+            break;
+
+            case "tile yellow":
+            this.blink(pieces[2]);
+              this.playSound(e)
+            break;
+
+            case "tile green":
+            this.blink(pieces[3]);
+              this.playSound(e)
+            break;
+        }
+      }
+    },
+
+    getRandomTile() {
+      switch (Math.floor(Math.random() * (4 - 1 + 1)) + 1) {
+        case 1:
+          return "tile red"
+
+        case 2:
+          return "tile blue"
+
+        case 3:
+          return "tile yellow"
+
+        case 4:
+          return "tile green"
+
+      }
+
+    },
+
+    playSound(step) {
+      switch (step) {
         case "tile red":
           soundOne.play()
           break;
@@ -63,8 +128,52 @@ export default {
 
       }
     },
-    startGame() {
 
+    blink(stepOrEvent) {
+      let area
+      if (this.isPlayer(stepOrEvent)) {
+        area = stepOrEvent.target;
+        console.log(area)
+      }else{
+        area = stepOrEvent
+        console.log(area)
+      }
+      console.log(area)
+      area.classList.add("blink")
+      setTimeout(function() {
+        return function() {
+          area.classList.toggle("blink");
+        };
+      }(), 150);
+    },
+
+    isPlayer(e) {
+      switch (e.target){
+        case undefined:
+          return false
+        default:
+          return true
+      }
+    },
+
+        startGame() {
+      // this.playerSteps = []
+      // this.simonsSteps = []
+
+      this.Simon()
+    },
+    Simon(){
+      let that = this;
+      let bup = that.getRandomTile()
+      that.simonsSteps.push(bup)
+      let steps = that.simonsSteps
+      for(let currentIndex=0; currentIndex<that.simonsSteps.length; currentIndex++) {
+        setTimeout(function() {
+          return function() {
+            that.action(bup)
+          };
+        }(currentIndex), that.getDifficulty()*(currentIndex));
+      }
     }
   }
 }
